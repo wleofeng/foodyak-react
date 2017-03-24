c74e84f28ffc90eac8466e5743225c3789970833
Leo Feng <wleofeng@gmail.com>
2017-03-23 21:54:23 -0400

    add input handler

--- components/search.js
+++ components/search.js
@@ -1,4 +1,24 @@
-export default () => (
-  <div>Restaurant Search</div>
-  test 
-)
+
+var React = require('react');
+
+module.exports = React.createClass({
+	constructor 
+
+
+  render: function() {
+    return (
+    	<form > 
+		    <input type="text" onChange={this.onChange.bind(this)}/>
+  			
+			  <button type="button">GO!</button>
+		  </form>
+    )
+  },
+
+  onChange: function(event) {
+  	var value = event.currentTarget.value;
+  	console.log(value);
+  }
+});
+
+
